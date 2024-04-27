47449b3b9e3664d57e32cfebb55b56a5f6377078
Hunter <hvanlear@gmail.com>
2024-04-27 12:25:31 -0400

    Update GridHero component and add Arrow asset

--- src/components/GridHero.tsx
+++ src/components/GridHero.tsx
@@ -15,7 +15,7 @@ export default function GridSmallBackgroundDemo() {
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div id="hero" className="">
         <h3 className="text-black">Welcome 🖖</h3>
-        <h1 className="text-4xl sm:text-7xl font-bold relative z-20 text-black py-8">
+        <h1 className="text-4xl sm:text-8xl font-bold relative z-20 text-black py-8">
           Toola
         </h1>
         <h2 className="text-black">Modern Technology Solutions</h2>
--- src/components/ui/Ticker.tsx
+++ src/components/ui/Ticker.tsx
@@ -1,11 +1,17 @@
 import Marquee from "react-fast-marquee";
+import { Arrow } from "../assets/Arrow";
 
 const services = ["UI Design", "UX Audit", "AI Consultancy", "Web Development"];
+
+const ticketGroup = services.flatMap((service, index) => [
+  service,
+  <Arrow key={index} />,
+]);
 
 export default function Ticker() {
   return (
     <Marquee>
-      {services.map((str, index) => (
+      {ticketGroup.map((str, index) => (
         <p className="text-4xl sm:text-7xl px-5 py-4 text-black" key={index}>
           {str}
         </p>
