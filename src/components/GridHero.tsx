ddd68bb50a64004893292e7421c9737504af64bb
Hunter <hvanlear@gmail.com>
2024-04-28 20:03:07 -0400

    Update GridHero component and fix Button component styles

--- src/components/GridHero.tsx
+++ src/components/GridHero.tsx
@@ -5,14 +5,16 @@ import { animationStyles } from "../utils/styleHelpers";
 import { useEffect } from "react";
 import Button from "./ui/Button";
 
-export default function GridSmallBackgroundDemo() {
+export default function GridHere() {
   const initialFadeIn = animationStyles.initialFadeIn;
 
   useEffect(() => {
     const staggerDelay = 0.2;
 
     document
-      .querySelectorAll("#hero h3, #hero h1, #hero h2, #heroButton")
+      .querySelectorAll(
+        "#hero h3,#byline, #hero h1, #hero h2, .button-container-hero"
+      )
       .forEach((item, i) => {
         animate(
           item,
@@ -25,23 +27,36 @@ export default function GridSmallBackgroundDemo() {
   return (
     <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-start pl-11">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div id="hero" className="pl-96 flex flex-col">
+      <div id="hero" className="space-y-4  flex-col">
         <div className="">
-          <h3 className="text-black text-3xl" style={initialFadeIn}>
-            Welcome 🖖
+          <h3
+            className="text-black text-3xl leading-snug"
+            style={initialFadeIn}
+          >
+            welcome 🖖
           </h3>
         </div>
 
         <h1
-          className=" sm:text-10xl font-bold relative z-20 text-black "
+          className="leading-none sm:text-10xl font-bold relative z-20 text-black "
           style={initialFadeIn}
         >
           Toola
         </h1>
-        <h2 className="text-black text-3xl" style={initialFadeIn}>
+        <h2
+          id="byline"
+          className="text-black text-3xl leading-snug"
+          style={initialFadeIn}
+        >
           Modern Technology Solutions
         </h2>
-        <Button id="heroButton" variant="lit" text="Say Hello!" />
+        <div
+          className="button-container-hero flex flex-row gap-4 items-center"
+          style={initialFadeIn}
+        >
+          <Button id="hero-button" variant="lit" text="Say Hello!" />
+          <span className="text-black">More Coming Soon</span>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({ variant, text, id }: ButtonProps) {
   const initialFadeIn = animationStyles.initialFadeIn;
   if (variant === "lit") {
     return (
-      <button id={id} className="p-[3px] relative" style={initialFadeIn}>
+      <button id={id} className="p-[3px] relative">
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
         <div className="font-bold px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
           {text}
@@ -24,7 +24,6 @@ export default function Button({ variant, text, id }: ButtonProps) {
   if (variant === "sketch") {
     return (
       <button
-        style={initialFadeIn}
         id={id}
         className="font-bold px-4 py-2 rounded-md border border-black bg-white text-neutral-700 text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
       >
