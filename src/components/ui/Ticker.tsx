1d59fcc926920d0c5cd3c1b214500e977e7972b8
Hunter <hvanlear@gmail.com>
2024-04-29 10:07:34 -0400

    Update Ticker component styles and add initialFadeIn animation

--- src/components/ui/Ticker.tsx
+++ src/components/ui/Ticker.tsx
@@ -1,6 +1,11 @@
 import Marquee from "react-fast-marquee";
 import { Arrow } from "../assets/Arrow";
 
+import { animate } from "framer-motion";
+import { motion } from "framer-motion";
+import { animationStyles } from "../../utils/styleHelpers";
+import { useEffect } from "react";
+
 interface TickerProps {
   tickerClass?: string;
 }
@@ -13,16 +18,35 @@ const ticketGroup = services.flatMap((service, index) => [
 ]);
 
 export default function Ticker({ tickerClass }: TickerProps) {
+  const initialFadeIn = animationStyles.initialFadeIn;
+
+  const variants = {
+    hidden: { opacity: 0, scale: 0, filter: "blur(10px)" },
+    visible: (i) => ({
+      opacity: 1,
+      scale: 1,
+      filter: "blur(0px)",
+      transition: {
+        delay: i * 0.2,
+        duration: 0.2,
+      },
+    }),
+  };
+
   return (
     <Marquee className={tickerClass}>
       {ticketGroup.map((str, index) => (
-        <p
-          id="ticker"
-          className="text-ticker-clamp  sm:text-7xl px-5 py-4 text-black font-serif font-light"
+        <motion.p
+          style={initialFadeIn}
+          className="ticker-item text-ticker-clamp sm:text-7xl px-5 py-4 text-black font-serif font-light"
           key={index}
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          custom={index}
         >
           {str}
-        </p>
+        </motion.p>
       ))}
     </Marquee>
   );
