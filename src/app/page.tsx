3132d917af1485d45cd72437d1e207adf6545846
Hunter <hvanlear@gmail.com>
2024-04-29 10:40:24 -0400

    SEO optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import Head from "next/head";
+
 import GridHero from "../components/Home/GridHero";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Toola Technical Solutions</title>
+        <meta
+          name="description"
+          content="Home Page for Toola Technical Solutions"
+        />
+        <meta property="og:title" content="Toola Technical Solutions" />
+        <meta
+          property="og:description"
+          content="Home Page for Toola Technical Solutions"
+        />
+      </Head>
       <GridHero />
     </>
   );
