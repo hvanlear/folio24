0d2caee268cd2bffce58d9d03506b575c98ab717
Hunter <hvanlear@gmail.com>
2024-05-01 13:06:42 -0400

    Add validationRules and validateForm functions to handle form validation in validateForm.ts

---
+++ src/utils/validateForm.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface Input {
+  name: string;
+  value: string;
+  validation?: (value: string) => string | null;
+}
+
+export const validationRules = {
+  name: (value: string) =>
+    !value || value.length < 3 ? "must be atleast 3 characters long" : null,
+  email: (value: string) =>
+    !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value)
+      ? "is not valid"
+      : null,
+  message: (value: string) =>
+    !value || value.length < 4 ? "must be at least 4 characters long" : null,
+};
+
+export function validateForm(
+  inputs: Input[],
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: () => void
+) {
+  for (const input of inputs) {
+    const { name, value, validation } = input;
+
+    //if validation function exists call and check
+    if (validation) {
+      const error = validation(req.body[value]);
+      if (error) {
+        return res.status(400).json({ message: `${name} ${error}` });
+      }
+    }
+  }
+
+  next();
+}
