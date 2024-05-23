865b0ac115731664fc5353f478fe99aca0ce187e
Hunter <hvanlear@gmail.com>
2024-05-23 10:46:53 -0400

    refactor: Remove unnecessary code and improve scroll animation in WorkSection.tsx

--- src/components/Work/WorkSection.tsx
+++ src/components/Work/WorkSection.tsx
@@ -8,27 +8,34 @@ export default function WorkAnimation() {
   const { scrollYProgress } = useScroll();
 
   // Calculate vertical movement based on scroll progress
-  // The range [-100, 100] determines how much the element moves vertically.
+  // The range [1, 0] determines how much the element moves vertically.
   // Adjust this range to fit the movement into your design.
-  const y = useTransform(scrollYProgress, [0, 1], [0, 200]);
+  const y = useTransform(scrollYProgress, [1, 0], [0, 200]);
 
   return (
-    <div
-      ref={containerRef}
-      style={{
-        height: "1000px",
-      }}
-    >
-      <motion.div
-        style={{
-          y, // Apply the dynamic vertical movement
-          // Ensure it's centered
-          fontSize: "10rem",
-          color: "#FF0088",
-        }}
-      >
-        Work
-      </motion.div>
-    </div>
+    <>
+      <div className="">
+        <div
+          ref={containerRef}
+          style={{
+            height: "1500px",
+            position: "relative",
+            overflow: "hidden",
+          }}
+        >
+          <motion.div
+            className="absolute top-0 left-0 w-full text-h1-clamp text-pink-600 z-10"
+            style={{
+              y, // Apply the dynamic vertical movement
+            }}
+          >
+            Work
+          </motion.div>
+          <div className="text-3xl text-black absolute top-[213px] left-0 z-10 ">
+            <p>Box</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
