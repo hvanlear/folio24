46757e3f0512feb4f46513fd456500e9c55c080b
Hunter <hvanlear@gmail.com>
2024-08-13 11:44:55 -0400

    add content to modeler

--- src/components/Project/Modeler.tsx
+++ src/components/Project/Modeler.tsx
@@ -1,7 +1,17 @@
 export default function Modeler() {
   return (
     <div>
-      <h2>CUSTOM CONTENT FOR BPMN</h2>
+      <h2 className="text-h3-clamp">The problem</h2>
+      <p className="text-2xl">
+        Our team faced the daunting task of overhauling our web-based BPMN
+        modeling canvas tool. Users struggled with a cumbersome interface,
+        limited maneuverability, and poor screen utilization. The inflexible
+        menu structure hindered feature integration and scalability. Compounding
+        these issues were a tight development timeline and a complex codebase
+        with potential performance pitfalls. Our challenge was to transform
+        these usability hurdles into a seamless, efficient user experience while
+        navigating technical constraints and time pressures
+      </p>
     </div>
   );
 }
