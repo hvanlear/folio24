5dbb16ee50e13a0d56b603c36e12231aac9fe148
Hunter <hvanlear@gmail.com>
2024-08-19 21:44:08 -0400

    adjust font size of archive slat to look better at smaller screen sizes

--- src/components/Project/DesignSystem.tsx
+++ src/components/Project/DesignSystem.tsx
@@ -1,8 +1,7 @@
-
 import {
   IconArrowBackUp,
   IconAdjustments,
-  IconDatabase,
+  IconDatabase
 
 } from "@tabler/icons-react";
 
@@ -15,36 +14,38 @@ export default function DesignSystem() {
       icon: IconArrowBackUp,
       title: "Reverse Engineering:",
       description:
-        "Extracted design information from the existing codebase to create a comprehensive system.",
+        "Extracting design information from the existing codebase to create a comprehensive system."
     },
     {
       icon: IconAdjustments,
       title: "Consistency Management:",
       description:
-        "Ensured the new system accurately represented the current UI while providing a foundation for future improvements",
+        "Ensuring the new system accurately represented the current UI while providing a foundation for future improvements."
     },
     {
       icon: IconDatabase,
       title: "Information Gathering",
       description:
-        "Compiled scattered design decisions into a cohesive system.",
-    },
+        "Compiling scattered design decisions into a cohesive system."
+    }
 
   ];
 
   return (
-    <div className="flex flex-col gap-24">
-      <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-24">π
+      < div className="flex flex-col gap-4">
         <ChallengeSection
-          overview="Developed a comprehensive foundational design system in Figma for a SaaS company, establishing a unified visual language and streamlining the design-to-development process."
+          overview="Developed a comprehensive foundational design system in Figma for a SaaS company,
+         establishing a unified visual language and streamlining the design-to-development process."
           challenges={challenges}
         />
       </div>
 
       <div className="flex flex-col gap-12">
         <h2 className="text-h2-clamp font-bold">Approach & Discovery</h2>
-        <p className="md:text-2xl text-lg">
-
+        <p className="md:text-2xl text-lg">Implemented Atomic Design principles. Worked backwards from existing UI to
+          ensure consistency with current product. Collaborated with product and development teams to align with live
+          implementation
         </p>
         <div className="w-full flex justify-center">
           {/* <Image
--- src/components/ui/ArchiveSlat.tsx
+++ src/components/ui/ArchiveSlat.tsx
@@ -9,10 +9,12 @@ export default function ArchiveSlat({ title, slug }: ArchiveSlatProps) {
     <Link href={`/archive/${slug}`} className="w-full">
       <div className="archive-slat group cursor-pointer transition-all duration-200 ease-in-out hover:bg-stone-950">
         <div className="flex justify-between items-center relative py-4 px-2 ">
-          <span className="text-2xl group-hover:font-bold text-stone-950 group-hover:text-slate-50 group-hover:translate-x-1 transition-all duration-300">
+          <span
+            className="text-small-clamp md:text-2xl group-hover:font-bold text-stone-950 group-hover:text-slate-50 group-hover:translate-x-1 transition-all duration-300">
             {title}
           </span>
-          <span className="text-sm group-hover:font-bold text-stone-950 group-hover:text-slate-50 group-hover:-translate-x-1 transition-all duration-300">
+          <span
+            className="text-small-clamp group-hover:font-bold text-stone-950 group-hover:text-slate-50 group-hover:-translate-x-1 transition-all duration-300">
             Full Case
           </span>
           <div className="absolute inset-x-0 -top-px h-px bg-stone-200 scale-x-100 group-hover:scale-x-0"></div>
