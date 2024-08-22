0c59d72e8cca637e81f6e72def65413f1e8c55b3
Hunter <hvanlear@gmail.com>
2024-08-22 09:31:02 -0400

    refactor: Update Logo component and DesignSystem page
    This commit updates the Logo component by adding a Link from the next/link package. The Logo component now wraps the SVG in a Link component, allowing users to navigate to the home page when clicking on the logo.
    
    Additionally, the DesignSystem page has been modified. The challenges array now includes a new challenge object with the IconDatabase icon. The DesignSystem page now displays three challenges, each with an icon, title, and description.
    
    These changes improve the functionality and user experience of the Logo component and enhance the content of the DesignSystem page.

--- src/components/Project/DesignSystem.tsx
+++ src/components/Project/DesignSystem.tsx
@@ -1,12 +1,10 @@
 import {
   IconArrowBackUp,
   IconAdjustments,
-  IconDatabase
-
+  IconDatabase,
 } from "@tabler/icons-react";
 
 import ChallengeSection from "./Parts/ChallengeSection";
-
 
 export default function DesignSystem() {
   const challenges = [
@@ -14,38 +12,38 @@ export default function DesignSystem() {
       icon: IconArrowBackUp,
       title: "Reverse Engineering:",
       description:
-        "Extracting design information from the existing codebase to create a comprehensive system."
+        "Extracting design information from the existing codebase to create a comprehensive system.",
     },
     {
       icon: IconAdjustments,
       title: "Consistency Management:",
       description:
-        "Ensuring the new system accurately represented the current UI while providing a foundation for future improvements."
+        "Ensuring the new system accurately represented the current UI while providing a foundation for future improvements.",
     },
     {
       icon: IconDatabase,
       title: "Information Gathering",
       description:
-        "Compiling scattered design decisions into a cohesive system."
-    }
-
+        "Compiling scattered design decisions into a cohesive system.",
+    },
   ];
 
   return (
-    <div className="flex flex-col gap-24">π
-      < div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-24">
+      π
+      <div className="flex flex-col gap-4">
         <ChallengeSection
           overview="Developed a comprehensive foundational design system in Figma for a SaaS company,
          establishing a unified visual language and streamlining the design-to-development process."
           challenges={challenges}
         />
       </div>
-
       <div className="flex flex-col gap-12">
         <h2 className="text-h2-clamp font-bold">Approach & Discovery</h2>
-        <p className="md:text-2xl text-lg">Implemented Atomic Design principles. Worked backwards from existing UI to
-          ensure consistency with current product. Collaborated with product and development teams to align with live
-          implementation
+        <p className="md:text-2xl text-lg">
+          Implemented Atomic Design principles. Worked backwards from existing
+          UI to ensure consistency with current product. Collaborated with
+          product and development teams to align with live implementation
         </p>
         <div className="w-full flex justify-center">
           {/* <Image
